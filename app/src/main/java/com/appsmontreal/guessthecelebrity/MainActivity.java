62949058ad4b8d2a269748831465fc47eab961ab
Marlon <escobar.marlon@gmail.com>
2019-05-09 14:54:46 -0400

    Facebook,email,Gmail authentication implemented with Firebase

--- app/src/main/java/com/appsmontreal/guessthecelebrity/MainActivity.java
+++ app/src/main/java/com/appsmontreal/guessthecelebrity/MainActivity.java
@@ -1,13 +1,90 @@
 package com.appsmontreal.guessthecelebrity;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
+    private static final int MY_REQUEST_CODE = 777;
+    List<AuthUI.IdpConfig> providers;
+    Button signOutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build()
+        );
+        signOutButton = findViewById(R.id.signOutButton);
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Logout
+                AuthUI.getInstance()
+                        .signOut(MainActivity.this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                signOutButton.setEnabled(false);
+                                showSignInOptions();
+                            }
+                        });
+
+            }
+        });
+        showSignInOptions();
+
+
+    }
+
+    private void showSignInOptions() {
+
+        startActivityForResult(
+                AuthUI.getInstance().createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setTheme(R.style.MyTheme)
+                .build(),MY_REQUEST_CODE
+        );
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == MY_REQUEST_CODE){
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK){
+                //get user
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                //Write email in toast
+                Toast.makeText(this,""+user.getEmail(),Toast.LENGTH_LONG).show();
+                signOutButton.setEnabled(true);
+            }else{
+                Toast.makeText(this,""+response.getError().getMessage(),Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
