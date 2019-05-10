2db560bd3631c0fc7a16bb5ec1b29bf47ae8028b
Marlon <escobar.marlon@gmail.com>
2019-05-09 23:15:36 -0400

    GameActivity setOnclickListener implementation

--- app/src/main/java/com/appsmontreal/guessthecelebrity/GameActivity.java
+++ app/src/main/java/com/appsmontreal/guessthecelebrity/GameActivity.java
@@ -2,12 +2,59 @@ package com.appsmontreal.guessthecelebrity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class GameActivity extends AppCompatActivity {
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class GameActivity extends AppCompatActivity implements View.OnClickListener {
+    FirebaseUser user;
+    TextView userTextView;
+    TextView scoreTextView;
+    int score;
+    Button buttons[] = new Button[6];
+    int buttonWidgets[] = {R.id.nameButton1,R.id.nameButton2,R.id.nameButton3,R.id.nameButton4,R.id.restartButton,R.id.exitButton};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        initialize();
+    }
+
+    private void initialize() {
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        userTextView = findViewById(R.id.userTextView);
+        userTextView.setText(user.getDisplayName());
+        scoreTextView = findViewById(R.id.scoreTextView);
+        resetScore();
+
+        for (int b = 0; b < buttons.length; b++){
+            buttons[b] = findViewById(buttonWidgets[b]);
+            buttons[b].setOnClickListener(this);
+        }
+    }
+
+    public void resetScore(){
+        score = 0;
+        scoreTextView = findViewById(R.id.scoreTextView);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.exitButton:
+                finish();
+                break;
+
+            case R.id.restartButton:
+                resetScore();
+
+        }
+
     }
 }
