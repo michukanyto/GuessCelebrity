1f882759f100d6ce08b6b7fac06e7e1d484d712b
Marlon <escobar.marlon@gmail.com>
2019-05-10 11:46:42 -0400

    WebContent class implemented

---
+++ app/src/main/java/Model/WebContent.java
@@ -0,0 +1,51 @@
+package Model;
+
+import android.os.AsyncTask;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class WebContent extends AsyncTask<String, Void, String> {
+
+    protected String result;
+    
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+       this.result = result;
+    }
+
+
+    @Override
+    protected String doInBackground(String... urls) {
+        URL url;
+        HttpURLConnection urlConnection = null;
+
+        try {
+            url = new URL(urls[0]);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            InputStream in = urlConnection.getInputStream();
+            InputStreamReader reader = new InputStreamReader(in);
+            int data = reader.read();
+
+            while (data != -1) {
+                char current = (char) data;
+
+                result += current;
+
+                data = reader.read();
+            }
+
+            return result;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            return "Failed";
+        }
+    }
+}
